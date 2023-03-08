330e6a96733259724126275c995ca4946f845160
Seven <lxw_623@163.com>
2023-03-08 23:13:51 +0800

    feat: 加载媒体与字体文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-// import "./App.css"
-
+import logo from './assets/images/logo.svg'
 // 将css转换成对象，再通过访问对象的形式来获取样式
 import style from './App.module.css'
 import robots from "./mockdata/robots.json"
@@ -9,6 +8,10 @@ import Robot from "./components/Robot"
 function App() {
   return (
     <div className={style.app}>
+      <div className={style.appHeader}>
+        <img src={logo} className={style.appLogo} alt="logo" />
+        <h1 className={style.headerFamily}>罗伯特机器人</h1>
+      </div>
       <div className={style.robotList}>
         {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
       </div>
