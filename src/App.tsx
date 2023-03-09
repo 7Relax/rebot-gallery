f53fa50cf412156adeec59e04366453d8d3cce0b
Seven <lxw_623@163.com>
2023-03-09 17:42:26 +0800

    feat: useEffect 使用指南

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,38 @@ import ShoppingCart from './components/ShoppingCart'
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0)
   const [robotGallery, setRobotGallery] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>('')
 
   useEffect(() => {
     document.title = `点击${count}次`
   }, [count])
 
   // 第二个参数是个空数组则代码在模拟类组件中的 componentDidMount
-  // 也就是只会在组件加载的时候调用一次，这种操作最适合获取数据初始化state
+  // 也就是只会在组件第一次渲染的时候调用，这种操作最适合获取数据初始化state
   // 如果不传第二个参数，它的副作用代码在每次页面渲染UI以后都会被执行一次
   // 而setRobotGallery又会更新robotGallery，UI组件会再次渲染，再次执行useEffect的副作用代码...
   // 所以不传第二个参数，则相当于在模拟 componentDidUpdate -> 即每次渲染结束后都会被调用
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setRobotGallery(data))
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await res.json()
+        setRobotGallery(data)
+      } catch (e) {
+        setError(e.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData() // 调用
   }, [])
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response => response.json())
+  //     .then(data => setRobotGallery(data))
+  // }, [])
 
   return (
     <div className={style.app}>
@@ -43,10 +60,16 @@ const App: React.FC = () => {
 
       <ShoppingCart />
 
-      <div className={style.robotList}>
-        { robotGallery.map(r =>
-          <Robot id={r.id} name={r.name} email={r.email} key={r.id} />) }
-      </div>
+      { (error !== '') && <div>网站出错：{error}</div> }
+
+      {
+        !loading ?
+        <div className={style.robotList}>
+          { robotGallery.map(r =>
+            <Robot id={r.id} name={r.name} email={r.email} key={r.id} />) }
+        </div>
+        : <h2>loading 加载中</h2>
+      }
 
     </div>
   )
